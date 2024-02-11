9639c883455efa1e67a85c29b6990c619fdd3d34
Albert Orlando <albertjorlando+git@gmail.com>
2024-02-11 11:38:51 -0500

    Update extraction

--- extraction/index.ts
+++ extraction/index.ts
@@ -1,26 +1,14 @@
 import {chromium} from "playwright-extra";
+
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
-// import UAPlugin from "puppeteer-extra-plugin-anonymize-ua";
 
 chromium.use(StealthPlugin());
-// chromium.use(UAPlugin());
 
 async function main() {
-    const credentials = {
-        email: "albertjorlando@gmail.com",
-        password: "uad*rfy0jcz0EBM0med"
-    };
-    const url = "https://members.junglescout.com/#/opportunity-finder";
-
-    const browser = await chromium.launch({
-        headless: false,
-        devtools: true,
-        slowMo: 1000,
-    });
-
+    const url = "https://nowsecure.nl";
+    const browser = await chromium.launch({headless: false});
     const page = await browser.newPage();
     await page.goto(url, {waitUntil: "networkidle"});
-
 }
 
 main().then(() => console.log("done"));
